16ac5cc8a8a8a937215e3b6e93fcb8768c3dd371
Alexander Lindsey <alexander.lindsey96@gmail.com>
2020-09-02 22:15:09 -0700

    Update 8-using_NULL.sql

--- 8-using_NULL.sql
+++ 8-using_NULL.sql
@@ -57,3 +57,17 @@ GROUP BY dept.name;
 
 
 
+--9. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+SELECT name, CASE WHEN dept IN (1, 2) THEN 'Sci' ELSE 'Art' END dept_name
+FROM teacher
+
+
+
+--10. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+SELECT name, 
+CASE 
+    WHEN dept IN (1, 2) THEN 'Sci'
+    WHEN dept=3 THEN 'ART'
+    ELSE 'None'
+    END dept_name
+FROM teacher
