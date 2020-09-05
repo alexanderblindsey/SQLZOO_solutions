b48dcf730ce4142e606305736c7bf3692f13de0f
Alexander Lindsey <alexander.lindsey96@gmail.com>
2020-09-05 12:23:23 -0700

    Update 9-Self_join.sql

--- 9-Self_join.sql
+++ 9-Self_join.sql
@@ -1,1 +1,20 @@
 -- 1. How many stops are in the database.
+SELECT COUNT(id)
+FROM stops;
+
+
+
+-- 2. Find the id value for the stop 'Craiglockhart'.
+SELECT id
+FROM stops
+WHERE name='Craiglockhart';
+
+
+
+-- 3. 
+SELECT id, name
+FROM stops
+JOIN route 
+  ON (route.stop=stops.id)
+WHERE route.num='4' 
+  AND route.company='LRT'
